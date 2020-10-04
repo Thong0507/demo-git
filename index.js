e08da7f276c117bf193549f10ab7452e9ae5b861
Thong0507 <can1nguoidehieu@gmail.com>
2020-10-04 10:17:26 +0700

    thay doi lan 456

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 console.log("GTT");
-// khởi tạo git: git init 
-// kết nối project vs repo: git remote add origin + link github của mình
-
-// kiểm tra trạng thái của source: git status: màu đỏ là đã thay đổi nhưng chưa save(working copy), xanh là đã lưu(staging area)
-
-// git add. là save tất cả các file, muốn save từng file thì git add + tên file (working copy => staging area)
-
 console.log("Demo-GIT")
 console.log("Demo-GIT234g")
