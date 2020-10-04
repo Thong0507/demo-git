70e6f416ef4cb3a02bae2d225cbc2bce882520b8
Thong0507 <72137541+Thong0507@users.noreply.github.com>
2020-10-04 09:28:06 +0700

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 console.log("GTT");
-// git init 
-// lệnh kết nối: git remote add origin + link github của mình
-//git add. là save tất cả các file, muốn save từng file thì git add + tên file
+// khởi tạo git: git init 
+// kết nối project vs repo: git remote add origin + link github của mình
+
+// kiểm tra trạng thái của source: git status: màu đỏ là đã thay đổi nhưng chưa save(working copy), xanh là đã lưu(staging area)
+
+// git add. là save tất cả các file, muốn save từng file thì git add + tên file (working copy => staging area)
